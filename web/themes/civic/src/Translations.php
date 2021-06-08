7cf3b8c992fedf74804227d3fd9185770a8f70ee
thomasfalcone90 <thomasfalcone90gmail.com>
2021-06-08 12:55:10 +0300

    #12886 Professional Translations: translations bugs fixes

--- web/themes/civic/src/Translations.php
+++ web/themes/civic/src/Translations.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\civic;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 class Translations {
 
   /**
@@ -10,15 +12,39 @@ class Translations {
   public static function processTranslations(&$vars) {
     // Get Default and Selected LangCodes
     $defaultLangCode = \Drupal::languageManager()->getDefaultLanguage()->getId();
-    
+
     // Set Selected LangCode if value is available
     $tempstore = \Drupal::service('user.private_tempstore')->get('allegheny_translation');
     $selectedLangCode = \Drupal::languageManager()->getCurrentLanguage()->getId();
 
+    $lns = \Drupal::service('language_negotiator');
+
+    $prefixes = \Drupal::config('language.negotiation')->get('url.prefixes');
+
+    if (!empty($prefixes['en'])) {
+      unset($prefixes['en']);
+
+      $prefixes = array_keys($prefixes);
+    }
+
+
+    $request_uri = $_SERVER['REQUEST_URI'];
+    
+    $uri_exploded = explode('/', $request_uri);
+
+    $url_langcode = 'en';
+
+    if (in_array($uri_exploded[1], $prefixes)) {
+      $url_langcode = $uri_exploded[1];
+    }
+
+    $selectedLangCode = $url_langcode;
+
     // If the LangCode is available, save it
     if ($selectedLangCode) {
       $tempstore->set('langcode', $selectedLangCode);
-    } else {
+    }
+    else {
       // If the LangCode is not available, check and see if one is saved.
       $selectedLangCode = $tempstore->get('langcode');
     }
@@ -41,16 +67,18 @@ class Translations {
         $vars['#attached']['drupalSettings']['proTranslationAvailable'] = $proTranslationAvailable;
 
         // Check if the current route is the pro translation for the selected lanague
-        $onProTranslation = \Drupal::languageManager()->getCurrentLanguage()->getId() == $selectedLangCode;
+        $onProTranslation = \Drupal::languageManager()->getCurrentLanguage()->getId() ==
+            $selectedLangCode;
 
         // If there is pro version and we're not viewing it, navigate to it.
         if ($proTranslationAvailable && !$onProTranslation) {
           $translation = $vars['node']->getTranslation($selectedLangCode);
           $url = $translation->toUrl('canonical')->toString();
           $response = new RedirectResponse($url, 302);
-          $response->send();
+        //  $response->send();
         }
       }
     }
   }
+
 }
